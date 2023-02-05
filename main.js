a80d59aa549578826d56807c4b0251c01f4aef11
hammibell <94499945+hammibell@users.noreply.github.com>
2023-02-05 17:37:07 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+screen_width = 0;
+screen_height= 0;
+
+apple = "";
+
+speak_data = "";
+
+to_number = Number(content);
+
+x = 0;
+y = 0;
+
+draw_apple = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "System is listening please speak";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
+
+}
+
+function preload() {
+  loadImage("apple.png");
+  apple = "apple.png"
+}
+
+function setup() {
+ 
+}
+
+function draw() {
+  if(draw_apple == "set")
+  {
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    draw_apple = "";
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
